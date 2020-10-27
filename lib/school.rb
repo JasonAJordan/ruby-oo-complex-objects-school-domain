f5852f3076175fa77840ba84f80ad4dbeeae8e9e
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-10-27 17:49:41 -0400

    testing

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School 
+
+    def initialize(name)
+        @name = name 
+        @roster = {}
+    end 
+
+    def roster
+        @roster
+    end 
+
+    def add_student=(student,grade)
+        if @roster[:grade] 
+            @roster[:grade].push(student)
+        else 
+            @roster[:grade] = student
+        end 
+    end 
+
+    def grade=(grade)
+        @roster[:grade]
+    end 
+    
+    def sort
+        @roster.sort
+    end 
+
+end 
